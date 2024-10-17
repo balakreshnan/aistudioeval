6c698073e6fdf2daa179aff1e168c58f9cb20d59
balakreshnan <babal@microsoft.com>
2024-10-17 09:41:35 -0500

    updated

--- rfpeval.py
+++ rfpeval.py
@@ -2,7 +2,7 @@ import pandas as pd
 import os
 
 from pprint import pprint
-from azure.ai.evaluation import evaluate
+from azure.ai.evaluation import evaluate, AzureAIProject, AzureOpenAIModelConfiguration
 from azure.identity import DefaultAzureCredential
 from azure.ai.evaluation import RelevanceEvaluator
 from azure.ai.evaluation import (
@@ -50,15 +50,19 @@ def main():
     
     # Load .env file
     load_dotenv()
-    citationtxt = extractrfpresults("Provide summary of Resources for Railway projects with 200 words?")
+    #citationtxt = extractrfpresults("Provide summary of Resources for Railway projects with 200 words?")
 
-    # print('Citation Text:', citationtxt)
+    #print('Citation Text:', citationtxt)
+    azure_endpoint = os.getenv("AZURE_OPENAI_ENDPOINT")
+    api_key = os.getenv("AZURE_OPENAI_API_KEY")
+    azure_deployment = os.getenv("AZURE_OPENAI_DEPLOYMENT")
+    api_version = os.getenv("AZURE_OPENAI_API_VERSION")
 
     model_config = {
-        "azure_endpoint": os.getenv("AZURE_OPENAI_ENDPOINT"),
-        "api_key": os.getenv("AZURE_OPENAI_API_KEY"),
-        "azure_deployment": os.getenv("AZURE_OPENAI_DEPLOYMENT"),
-        "api_version": os.getenv("AZURE_OPENAI_API_VERSION"),
+        "azure_endpoint": azure_endpoint,
+        "api_key": api_key,
+        "azure_deployment": azure_deployment,
+        "api_version": api_version,
     }
 
 
@@ -69,63 +73,79 @@ def main():
         print(ex)
 
 
-    azure_ai_project={
-        "subscription_id": os.getenv("AZURE_SUBSCRIPTION_ID"),
-        "resource_group_name": os.getenv("AZURE_RESOURCE_GROUP"),
-        "project_name": os.getenv("AZUREAI_PROJECT_NAME"),
-        # "azure_crendential": credential,
+    # azure_ai_project={
+    #     "subscription_id": os.getenv("AZURE_SUBSCRIPTION_ID"),
+    #     "resource_group_name": os.getenv("AZURE_RESOURCE_GROUP"),
+    #     "project_name": os.getenv("AZUREAI_PROJECT_NAME"),
+    #     # "azure_crendential": credential,
+    # }
+    subscription_id = os.getenv("AZURE_SUBSCRIPTION_ID")
+    resource_group_name = os.getenv("AZURE_RESOURCE_GROUP")
+    project_name = os.getenv("AZUREAI_PROJECT_NAME")
+    print(subscription_id, resource_group_name, project_name)
+    azure_ai_project = AzureAIProject(subscription_id=subscription_id, 
+                                      resource_group_name=resource_group_name, 
+                                      project_name=project_name, 
+                                      azure_crendential=credential)
+    
+    azure_ai_project_dict = {
+        "subscription_id": subscription_id,
+        "resource_group_name": resource_group_name,
+        "project_name": project_name,
+        "azure_credential": credential
     }
+    
 
-    relevance_evaluator = RelevanceEvaluator(model_config)
+    # relevance_evaluator = RelevanceEvaluator(model_config)
 
-    relevance_evaluator(
-        response=citationtxt,
-        context="summary of Resources for Railway projects.",
-        query="Provide summary of Resources for Railway projects with 200 words?",
-    )
-    pprint(relevance_evaluator)
+    # relevance_evaluator(
+    #     response="Virginia railway express RFP need introduction, resources.",
+    #     context="summary of Resources for Railway projects.",
+    #     query="Provide summary of Resources for Railway projects with 200 words?",
+    # )
+    # pprint(relevance_evaluator)
 
     # prompty_path = os.path.join("./", "rfp.prompty")
-    content_safety_evaluator = ContentSafetyEvaluator(azure_ai_project)
+    content_safety_evaluator = ContentSafetyEvaluator(azure_ai_project=azure_ai_project_dict, credential=credential)
     relevance_evaluator = RelevanceEvaluator(model_config)
     coherence_evaluator = CoherenceEvaluator(model_config)
     groundedness_evaluator = GroundednessEvaluator(model_config)
     fluency_evaluator = FluencyEvaluator(model_config)
-    similarity_evaluator = SimilarityEvaluator(model_config)
+    # similarity_evaluator = SimilarityEvaluator(model_config)
 
-    # results = evaluate(
-    #     evaluation_name="rfpevaluation",
-    #     data="datarfp.jsonl",
-    #     target=extractrfpresults,
-    #     #evaluators={
-    #     #    "relevance": relevance_evaluator,
-    #     #},
-    #     #evaluator_config={
-    #     #    "relevance": {"response": "${target.response}", "context": "${data.context}", "query": "${data.query}"},
-    #     #},
-    #     evaluators={
-    #     #    "content_safety": content_safety_evaluator,
-    #     #    "coherence": coherence_evaluator,
-    #         "relevance": relevance_evaluator,
-    #     #    "groundedness": groundedness_evaluator,
-    #     #    "fluency": fluency_evaluator,
-    #     #    "similarity": similarity_evaluator,
-    #     },        
-    #     evaluator_config={
-    #         "content_safety": {"query": "${data.query}", "response": "${target.response}"},
-    #         "coherence": {"response": "${target.response}", "query": "${data.query}"},
-    #         "relevance": {"response": "${target.response}", "context": "${data.context}", "query": "${data.query}"},
-    #         "groundedness": {
-    #             "response": "${target.response}",
-    #             "context": "${data.context}",
-    #             "query": "${data.query}",
-    #         },
-    #         "fluency": {"response": "${target.response}", "context": "${data.context}", "query": "${data.query}"},
-    #         "similarity": {"response": "${target.response}", "context": "${data.context}", "query": "${data.query}"},
-    #     },
-    #     azure_ai_project=azure_ai_project,
-    #     output_path="./rsoutputmetrics.json",
-    # )
+    results = evaluate(
+        evaluation_name="rfpevaluation",
+        data="datarfp.jsonl",
+        target=extractrfpresults,
+        #evaluators={
+        #    "relevance": relevance_evaluator,
+        #},
+        #evaluator_config={
+        #    "relevance": {"response": "${target.response}", "context": "${data.context}", "query": "${data.query}"},
+        #},
+        evaluators={
+            "content_safety": content_safety_evaluator,
+            "coherence": coherence_evaluator,
+            "relevance": relevance_evaluator,
+            "groundedness": groundedness_evaluator,
+            "fluency": fluency_evaluator,
+        #    "similarity": similarity_evaluator,
+        },        
+        evaluator_config={
+            "content_safety": {"query": "${data.query}", "response": "${target.response}"},
+            "coherence": {"response": "${target.response}", "query": "${data.query}"},
+            "relevance": {"response": "${target.response}", "context": "${data.context}", "query": "${data.query}"},
+            "groundedness": {
+                "response": "${target.response}",
+                "context": "${data.context}",
+                "query": "${data.query}",
+            },
+            "fluency": {"response": "${target.response}", "context": "${data.context}", "query": "${data.query}"},
+            "similarity": {"response": "${target.response}", "context": "${data.context}", "query": "${data.query}"},
+        },
+        azure_ai_project=azure_ai_project,
+        output_path="./rsoutputmetrics.json",
+    )
     # pprint(results)
     # parse_json(results)
     print("Done")
